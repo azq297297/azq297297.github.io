5f3f423ab7fe4498c1040b643e8779af490d59ce
azq297297 <1845253678@qq.com>
2017-12-13 08:01:16 +0800

    第1，2动态完成

--- weichangjing/js/script.js
+++ weichangjing/js/script.js
@@ -12,7 +12,30 @@ var swiper = new Swiper('.swiper-container', {
     navigation: {
         nextEl: '.swiper-button-next',
     },
+    
+    //动画效果
+    on:{
+      init: function(){
+        swiperAnimateCache(this); //隐藏动画元素 
+        swiperAnimate(this); //初始化完成开始动画
+      }, 
+      slideChangeTransitionEnd: function(){ 
+        swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+      } 
+    }
 });
 
 
+
+var mp3 = document.querySelector("#mp3s");
+var mp3but = document.querySelector("#mp3");
+mp3but.addEventListener('click',function(){
+    if(mp3.paused){
+        mp3.play();
+        mp3but.style.animationPlayState= "running";
+    }else{
+        mp3.pause();
+        mp3but.style.animationPlayState= "paused";
+    }
+});
 
