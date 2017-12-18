819510c2a3953185a656e0b3f85e5b8b0ef05667
P\lenovo <1845253678@qq.com>
2017-12-18 20:07:59 +0800

    no message

---
+++ personal/js/scirpt.js
@@ -0,0 +1,10 @@
+var wh=$(window).height();
+$(window).scroll(function(){
+var s=wh-$(window).scrollTop();
+if(s<652){
+console.log(s);
+    $(".navbar-default").css('background',"#fff");
+}else{
+     $(".navbar-default").css('background',"none");
+}
+});
